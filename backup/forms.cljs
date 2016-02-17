6f6fa03571106eb50b1ec1e0aa8fe179c7df40bd
ingesolvoll <inge.solvoll@gmail.com>
2016-02-17 11:01:26 +0100

    More templates to copy

--- backup/forms.cljs
+++ backup/forms.cljs
@@ -9,3 +9,21 @@
                          :aria-valuemin "0"
                          :aria-valuemax "100"
                          :style         {:width percent}} percent])])
+
+(defn required [value]
+  (not (str/blank? value)))
+
+(defn validate-int [value]
+  (-> value
+      js/parseInt
+      js/isNaN
+      not))
+
+(defn text-input [label key state]
+  [:div
+   [:label label]
+   [:input.form-control {:type  :text
+                         :value (key @state)
+                         :on-change
+                                #(swap! state assoc
+                                        key (-> % .-target .-value))}]])
