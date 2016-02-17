623cfc49c8ad4ceba5822298b5320514772e3899
ingesolvoll <inge.solvoll@gmail.com>
2016-02-17 12:06:50 +0100

    Nice starting point for forms

--- src/meetup/forms.cljs
+++ src/meetup/forms.cljs
@@ -2,57 +2,19 @@
   (:require [reagent.ratom :refer-macros [reaction]]
             [clojure.string :as str]))
 
-(defn input-field [label key state]
+(defn form [state]
   [:div
-   [:label label]
-   [:input.form-control {:type  :text
-                         :value (key @state)
-                         :on-change
-                                #(swap! state assoc
-                                        key (-> % .-target .-value))}]])
-
-(defn floor [n]
-  (.floor js/Math n))
-
-(defn required [input]
-  (not (str/blank? input)))
-
-(defn valid-integer? [input]
-  (not (js/isNaN (js/parseInt input))))
-
-(defn validate-field [validations [key value]]
-  (if-let [validation-fn (key validations)]
-    (validation-fn value)
-    true))
-
-(defn success-count [state validations]
-  (->> state
-       (map (partial validate-field validations))
-       (filter true?)
-       count))
-
-(defn automatic-field [state index key]
-  ^{:key index}
-  [input-field (name key) key state])
-
-(defn completion-as-percentage [state validations]
-  (-> (success-count @state validations)
-      (/ (count validations))
-      (* 100)
-      (floor)))
-
-(defn progress [state validations]
-  [:div.progress
-   (let [percent (str (completion-as-percentage state validations) "%")]
-     [:div.progress-bar {:class         (if (= percent "100%")
-                                          :progress-bar-success
-                                          :progress-bar-warning)
-                         :role          :progress-bar
-                         :aria-valuenow percent
-                         :aria-valuemin "0"
-                         :aria-valuemax "100"
-                         :style         {:width percent}} percent])])
-
-(defn registration [state]
-  [:div.form-group
-   (map-indexed (partial automatic-field state) (keys @state))])
+   [:div
+    [:label "Fornavn"]
+    [:input.form-control {:type  :text
+                          :value (:first-name @state)
+                          :on-change
+                                 #(swap! state assoc
+                                         :first-name (-> % .-target .-value))}]]
+   [:div
+    [:label "Etternavn"]
+    [:input.form-control {:type  :text
+                          :value (:last-name @state)
+                          :on-change
+                                 #(swap! state assoc
+                                         :first-name (-> % .-target .-value))}]]])
